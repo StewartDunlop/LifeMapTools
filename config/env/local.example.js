216bc5a9a9956ecb7ad5ed06a5d9a63ffdc49b12
Stewart Dunlop <stewartdunlopuk@gmail.com>
2015-12-30 12:31:47 +0000

    Revert "added google oauth2"
    This reverts commit 99a5146d2f11c6e12013fe7ec5ab18d15d364280.

--- config/env/local.example.js
+++ config/env/local.example.js
@@ -4,14 +4,21 @@
 // will not get commited and pushed to remote repositories.
 // Use it for your API keys, passwords, etc.
 
-
+/* For example:
 
 module.exports = {
-    sessionSecret: process.env.SESSION_SECRET || 'intervalsecrethorsestapleeat',
-    google: {
-        clientID: '505684028880-qjnrns0koa5aait14cbc9kdf4l9igclt.apps.googleusercontent.com',
-        clientSecret: 'aeflGiLsfATOAIx_5Qwl6kJW',
-        callbackURL: '/auth/google/callback'
+  db: {
+    uri: 'mongodb://localhost/local-dev',
+    options: {
+      user: '',
+      pass: ''
     }
+  },
+  sessionSecret: process.env.SESSION_SECRET || 'youshouldchangethistosomethingsecret',
+  facebook: {
+    clientID: process.env.FACEBOOK_ID || 'APP_ID',
+    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    callbackURL: '/api/auth/facebook/callback'
+  }
 };
-
+*/
